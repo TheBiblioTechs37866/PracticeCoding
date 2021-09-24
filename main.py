0f1f839bd9d5a723be40c1b3f069caec6acfc36b
BiblioTech-Tau <bibliotechtau@gmail.com>
2021-09-24 15:22:51 -0400

    I made a line following code

--- main.py
+++ main.py
@@ -6,50 +6,23 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import random
+
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
-WHEEL_DIAMETER = 56
-
-# FUNCTIONS HERE
-def inches_to_mm(inches):
-    return inches * 25.4
-
-def sec_to_ms(seconds):
-    MS = 1000
-    return seconds * MS
-
-def robot_stop():
-    robot.stop()
-    left_motor.brake()
-    right_motor.brake()
-    wait(seconds(0.25))
-
-def straight_left_straight(distance1, angle, distance2):
-    robot.straight(inches_to_mm(distance1))
-    random.randint(sec_to_ms(1), sec_to_ms(10))
-    robot.turn(angle)
-    random.randint(sec_to_ms(1), sec_to_ms(10))
-    robot.straight(inches_to_mm(distance2))
-    
 
 
-# OBJECTS
+# Create your objects here.
 ev3 = EV3Brick()
 left_motor = Motor(Port.D)
 right_motor = Motor(Port.C)
-front_motor = Motor(Port.A)
-robot = DriveBase(left_motor, right_motor, WHEEL_DIAMETER, 140)
-robot.settings(200, 100, 150, 100)
-gyro = GyroSensor(Port.S4)
+robot = DriveBase(left_motor,right_motor,wheel_diameter=56, axle_track=121)
+light = ColorSensor(Port.S1)
 
-# PROGRAM
-straight_left_straight(12, 45, 12)
-two_inches = inches_to_mm(2)
-robot.straight(two_inches)
-robot.turn(90)
-robot.straight(two_inches)
-
-#This is a change
-#This is another change
+# Write your program here.
+while robot.distance() >= 1000:
+    correction = (30 - light.reflection())*2
+    robot.drive(100,correction)
+robot.stop()
+left_motor.brake()
+right_motor.brake()
